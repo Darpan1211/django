4a30d4bdea8f7e4f52d893267def6875ca127f1e
Darpan1211 <darpan.innovate@gmail.com>
2023-06-12 12:15:19 +0530

    Template Add and Navbar link set

--- home/views.py
+++ home/views.py
@@ -3,9 +3,13 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     context = {
-        'variable':"This is text"
+        "variable":"my name is darpan",
+        "name":'darpan'
     }
     return render(request, 'index.html', context)
 
 def about(request):
     return HttpResponse("This is a about")
+
+def contact(request):
+    return HttpResponse("This is a Contact page")
