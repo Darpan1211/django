c140e4aa37c2173451ede2d8c1e0a5c30952ed34
Darpan1211 <darpan.innovate@gmail.com>
2023-06-12 15:44:49 +0530

    compalate website create

--- home/views.py
+++ home/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
-
+     return render(request, 'index.html')
+    
 def about(request):
     return render(request, 'about.html')
 
 def contact(request):
-   return render(request, 'contact.html')
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        city = request.POST.get('city')
+        contact = Contact(name=name, email=email, city=city, date = datetime.today())
+        contact.save()
+        messages.success(request, "Your Message has been sent.")
+
+    return render(request, 'contact.html')
+
 def service(request):
-    
-   return render(request, 'service.html')
+      return render(request, 'service.html')
